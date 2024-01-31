f51618706f81ee676310c4f943f042160a7b3946
gians1821 <gian.samana.1801@gmail.com>
2024-01-31 14:21:12 -0500

    docs: project started

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="Yellow">
+      <div className="bg-yellow-500"></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,jsx,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "Yellow": "hsl(47, 88%, 63%)",
+      }
+    },
+  },
+  plugins: [],
+}
+
